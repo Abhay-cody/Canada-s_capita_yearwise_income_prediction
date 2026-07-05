3ef195f5e37f70aa36169271614a4a8c4809b5a9
Abhay-cody <abhaykumargupta12312@gmail.com>
2026-07-05 15:13:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,3 @@
-#Canada's_capital_yearwise_income_prediction
-
-#Import Libraries and Load Data
-
-#import the libraries
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -107,75 +102,6 @@ st.sidebar.markdown("""
 
 <br><br>
 <hr>
-
-⭐ Thank you for visiting this Machine Learning Project.
-
-</div>
-""", unsafe_allow_html=True)
-
-
-# Load the dataset
-df = pd.read_csv('canada_per_capita_income.csv')
-
-# Let's look at the first few rows to understand the structure
-print(df.head())
-
-#Visualize the Data
-
-plt.xlabel('Year')
-plt.ylabel('Per Capita Income (US$)')
-plt.scatter(df['year'], df['per capita income (US$)'], color='red', marker='+')
-plt.show()
-
-# X must be a 2D array/dataframe
-X = df[['year']]
-
-# y can be a 1D series
-y = df['per capita income (US$)']
-
-# Create linear regression object
-reg = linear_model.LinearRegression()
-
-# Train the model using the training sets
-reg.fit(X, y)
-
-# Predict the income for the year 2020
-predicted_income = reg.predict([[2020]])
-print(f"Predicted per capita income for 2020: {predicted_income[0]}")
-
-# Show the slope (m)
-print("Coefficient (m):", reg.coef_)
-
-# Show the intercept (b)
-print("Intercept (b):", reg.intercept_)
-
-# Plotting the regression line
-plt.plot(df['year'], reg.predict(df[['year']]), color='blue', label='Regression Line')
-plt.legend()
-plt.show()
-
-# ==============================
-# Sidebar Branding
-# ==============================
-
-st.markdown("---")
-
-st.markdown("""
-<div style='text-align:center;'>
-
-### 🚀 Developed by <span style="color:#4CAF50;">ABHAY KUMAR GUPTA</span>
-
-<a href="https://github.com/Abhay-cody" target="_blank">
-<img src="https://img.shields.io/badge/GitHub-181717?style=for-the-badge&logo=github&logoColor=white">
-</a>
-
-&nbsp;
-
-<a href="https://www.linkedin.com/in/abhay-kumar-gupta-104a18397" target="_blank">
-<img src="https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white">
-</a>
-
-<br><br>
 
 ⭐ Thank you for visiting this Machine Learning Project.
 
